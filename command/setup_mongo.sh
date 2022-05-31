8ad07ee35edc2f98504a0293fdf5351dace6370f
Aravind M <34851872+the-code-innovator@users.noreply.github.com>
2022-06-01 01:53:40 +0530

    changes

---
+++ command/setup_mongo.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+export MONGO_URL=mongodb://localhost/squeezer
+export MONGO_TIMEOUT=30
+export MONGO_DB=squeezer
+export URL_DB=mongo
---
+++ command/setup_redis.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+export URL_DB=redis
+export REDIS_URL=redis://localhost:6379
